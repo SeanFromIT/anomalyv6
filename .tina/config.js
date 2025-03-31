d7d2282d05779bcbdc207cb0397a9a4e054549fa
Sean Feeney <SeanFromIT@gmail.com>
2025-03-31 11:55:28 -0700

    initial commit

---
+++ .tina/config.js
@@ -0,0 +1,79 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID, // Get this from tina.io
+  token: process.env.TINA_TOKEN, // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "assets/img",
+      publicFolder: "./",
+    },
+  },
+  search: {
+    tina: {
+      indexerToken: process.env.SEARCH_TOKEN, // Get this from tina.io
+      stopwordLanguages: ['eng']
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100
+  },
+  schema: {
+    collections: [
+      {
+        name: "link",
+        label: "Links",
+        path: "_links",
+        defaultItem: () => {
+          return {
+            date: new Date().toISOString(),
+          }
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title1",
+            label: "Title1",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "url1",
+            label: "URL1",
+            required: true
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "categories",
+            label: "categories",
+            list: true,
+          },
+          {
+            type: "string",
+            name: "title2",
+            label: "Title2",
+          },
+          {
+            type: "string",
+            name: "url2",
+            label: "URL2",
+          },
+        ],
+      },
+    ],
+  },
+});
